492455e594ddf926fa33af294f2b7ae9a671bdbf
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-01-25 15:11:52 +0100

    feat: first UML implementation

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -1,4 +1,24 @@
 package com.jad;
 
+import java.util.ArrayList;
+
 public class Foo {
+    private Bar bar;
+    private ArrayList<Baz> bazes = new ArrayList<>();
+    private Qux qux;
+    private Corge corge;
+    private ArrayList<Grault> graults = new ArrayList<>();
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+        this.qux = new Qux();
+    }
+
+    public void addBaz(Baz baz) {
+        this.bazes.add(baz);
+    }
+
+    public void addGrault() {
+        this.graults.add(new Grault(this));
+    }
 }
--- src/test/java/com/jad/Grault.java
+++ src/test/java/com/jad/Grault.java
@@ -1,4 +1,9 @@
 package com.jad;
 
 public class Grault {
+    private final Foo foo;
+
+    Grault(Foo foo) {
+        this.foo = foo;
+    }
 }
