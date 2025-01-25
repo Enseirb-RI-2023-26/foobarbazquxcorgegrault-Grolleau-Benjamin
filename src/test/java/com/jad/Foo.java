d5e4b0867ff636609430cfea418def799dfe4068
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-01-25 15:33:48 +0100

    fix{Foo}: fix setCorge to don't call function on null element

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -19,7 +19,7 @@ public class Foo {
     }
 
     public void setCorge(Corge corge) {
-        this.corge.setFoo(null);
+        if (this.corge != null) this.corge.setFoo(null);
         this.corge = corge;
     }
 
